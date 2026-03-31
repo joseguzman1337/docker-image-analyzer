7fce62625b96627bb762e3858ef78081c14fc061
jose guzman <joseguzman1337@users.noreply.github.com>
2026-03-31 00:51:13 -0500

    [CRUSH] chore(repo): checkpoint pending changes #TASK-000

--- src/main/java/com/rapid7/container/analyzer/docker/analyzer/ImageHandler.java
+++
@@ -1,9 +0,0 @@
-package com.rapid7.container.analyzer.docker.analyzer;
-
-import com.rapid7.container.analyzer.docker.model.image.Image;
-import java.io.IOException;
-
-public interface ImageHandler {
-
-  void handle(Image image) throws IOException;
-}
--- src/main/java/com/rapid7/container/analyzer/docker/model/image/PackageValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.rapid7.container.analyzer.docker.model.image;
-
-public class PackageValidationException extends Exception {
-  public PackageValidationException(String errorMessage) {
-    super(errorMessage);
-  }
-}
